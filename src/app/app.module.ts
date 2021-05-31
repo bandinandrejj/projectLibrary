221d7fbebb00893d70b5c5b4277d464e08abb76b
Andrew B <bandinandrejj@yandex.ru>
2021-05-31 18:55:07 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {AuthModule} from "./autorisation/auth.module";
+import {HttpClientModule} from "@angular/common/http";
+import {environment} from "../environments/environment";
+import {AngularFireModule} from "@angular/fire";
+import {AngularFireDatabaseModule} from "@angular/fire/database";
+import {StudentMainComponent} from './student/student-main/student-main.component';
+import {Routes, RouterModule} from '@angular/router';
+import {AutorisationComponent} from "./autorisation/auth/autorisation.component";
 
-import { AppComponent } from './app.component';
+
+const routes: Routes =[
+  { path: '', component: AutorisationComponent},
+  { path: 'student', component: StudentMainComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentMainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/autorisation/student.interface.ts
@@ -0,0 +1,23 @@
+export class student {
+  constructor(
+    public userLogin: string,
+    public userPass: string,
+  ) {}
+}
+
+export interface authStudent { // Не копия ли это нижнего интерфейса, но только для одного элемента.
+  key: string | null,
+  userLogin: string,
+  userPass: string,
+  userName: string,
+  userLastName: string,
+  userFlag: string,
+}
+
+
+
+export interface students {
+  students: student[]
+}
+
+
